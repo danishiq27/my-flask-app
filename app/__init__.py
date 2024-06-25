826f3d9e756d96ed2bbc093959452ec49f00f0df
ubuntu <ubuntu@do-university-3>
2024-06-25 13:35:42 +0000

    latest commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+import pymysql
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,6 +13,9 @@ login.login_view = 'login'
 def create_app(config_class=Config):
     app = Flask(__name__, static_folder='static')
     app.config.from_object(config_class)
+
+   # Configure SQLAlchemy to use pymysql
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_user:password@localhost/flask_db'    
 
     db.init_app(app)
     migrate.init_app(app, db)
